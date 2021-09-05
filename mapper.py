3ed0f7e139bf8e0c117c6dd0e9e442f8f2d969d7
Kamal Khatiwada <kamalkhatiwada21@gmail.com>
2021-09-05 03:51:23 +0000

    lowecase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 import sys
+import string
+
+stop_words = ['the', 'and']
+
+#mapping punctuation to whitespace
+#https://stackoverflow.com/questions/34860982/replace-the-punctuation-with-whitespace
+translator = string.maketrans(string.punctuation, ' '*len(string.punctuation))
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
-
+    line = line.strip().lower()
+ #   line = line.translate(translator)
+    line = line.translate(translator)
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
+#    stopWords = set (['the', 'and'])
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stop_words:
+           print '%s\t%s' % (word, "1")
